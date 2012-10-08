c953c9f57093450a163c82b36e9359b75c5e380c
ChrisRenton <chris@renton.com.au>
2012-10-08 11:17:34 +0800

    Update assets/www/CORDOVA_GCM_script.js

--- assets/www/CORDOVA_GCM_script.js
+++ assets/www/CORDOVA_GCM_script.js
@@ -33,7 +33,7 @@ document.addEventListener('deviceready', function() {
   // CHANGE: your_app_id
   // TO: what ever your GCM authorized senderId is
   //
-  window.GCM.register("goatus@gmail.com", "GCM_Event", GCM_Success, GCM_Fail );
+  window.GCM.register("157390337510", "GCM_Event", GCM_Success, GCM_Fail );
 
 }, false );
 
